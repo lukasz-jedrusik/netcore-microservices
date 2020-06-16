b9f6d49a33df4ad4cedb5da12f632df1a278816a
Łukasz Jędrusik <ljedrusik84@gmail.com>
2020-06-16 17:55:03 +0200

    sec#4-4 API/Startup.cs mvc version 2.2

--- src/Actio.API/Startup.cs
+++ src/Actio.API/Startup.cs
@@ -28,7 +28,7 @@ namespace Actio.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddRabbitMq(Configuration);
 
